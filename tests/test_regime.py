5e75ca073ee95893af67d90fe20388018e090856
Masoud Kashani <salar0033@gmail.com>
2026-08-15 17:17:52 +0330

    Delete tests directory

--- tests/test_regime.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from regime import get_regime, REGIME_RANGE
-
-def test_get_regime_dummy():
-    """
-    با دیتافریم خالی، باید RANGE برگرداند.
-    """
-    result = get_regime(pd.DataFrame())
-    assert result == REGIME_RANGE
